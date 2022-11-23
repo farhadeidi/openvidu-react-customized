c88dbc0e149bc9e304ec1963a283f05c2b25b494
Farhad Eidi <farcom7@gmail.com>
2022-11-23 21:38:51 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _VideoRoomComponent = _interopRequireDefault(require("./components/VideoRoomComponent"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var _default = _VideoRoomComponent["default"];
+exports["default"] = _default;
